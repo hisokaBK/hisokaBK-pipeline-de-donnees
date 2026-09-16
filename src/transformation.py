0b105df652ef6764d37df84f1441377ccee57f75
hisokaBK <bilalbakessou99@gmail.com>
2026-09-16 16:32:56 +0100

    feat: détection des doublons, des incohérences et contrôle de la qualité des données

--- src/transformation.py
+++ src/transformation.py
@@ -28,8 +28,55 @@ for i, data in enumerate(weather):
 
 weather_df = pd.DataFrame(weather_rows)
 
-print(weather_df.dtypes)
+#Standardiser les types et les dates-------------
 weather_df["date"] = pd.to_datetime(weather_df["date"])
-print(weather_df.dtypes)
+#print(weather_df.dtypes)
+
+#detecter les doublons----------------
+doublons = weather_df.duplicated(
+    subset=["city", "date"]
+)
+print("Nombre de doublons :", doublons.sum())
+
+#detecter les incoherences
+temperature_incoherente = (
+    weather_df["temperature_min"] > weather_df["temperature_max"]
+)
+
+print(
+    "Températures incohérentes :",
+    temperature_incoherente.sum()
+)
+
+precipitation_incoherente = (
+    weather_df["precipitation"] < 0
+)
+
+print(
+    "Précipitations négatives :",
+    precipitation_incoherente.sum()
+)
+
+
+latitude_incoherente = (
+    (weather_df["latitude"] < -90) |
+    (weather_df["latitude"] > 90)
+)
+
+longitude_incoherente = (
+    (weather_df["longitude"] < -180) |
+    (weather_df["longitude"] > 180)
+)
+
+print("Latitudes incohérentes :", latitude_incoherente.sum())
+print("Longitudes incohérentes :", longitude_incoherente.sum())
+
+
+print("Valeurs manquantes :" , weather_df.isna().sum())
+
+print("Nombre de lignes :", len(weather_df))
+print("Nombre de villes :", weather_df["city"].nunique())
+print("Colonnes :", weather_df.columns.tolist())
+
 
 
