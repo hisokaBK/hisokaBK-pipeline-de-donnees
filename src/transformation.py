56af1be8c37503309fc3f0cbf15f368481159149
hisokaBK <bilalbakessou99@gmail.com>
2026-09-16 16:06:16 +0100

    feat: récupération des données des fichiers cities.csv et weather.json et réalisation de la jointure sous forme de DataFrame

--- src/transformation.py
+++ src/transformation.py
@@ -6,3 +6,30 @@ cities = pd.read_csv('data/bronze/cities.csv')
 with open("data/bronze/weather.json" , 'r' , encoding='utf-8') as fichier :
     weather = json.load(fichier)
 
+
+weather_rows = []
+
+for i, data in enumerate(weather):
+
+    ville = cities.iloc[i]
+
+    daily = data["daily"]
+
+    for j in range(len(daily["time"])):
+        weather_rows.append({
+            "city": ville["city"],
+            "latitude": data["latitude"],
+            "longitude": data["longitude"],
+            "date": daily["time"][j],
+            "temperature_max": daily["temperature_2m_max"][j],
+            "temperature_min": daily["temperature_2m_min"][j],
+            "precipitation": daily["precipitation_sum"][j]
+        })
+
+weather_df = pd.DataFrame(weather_rows)
+
+print(weather_df.dtypes)
+weather_df["date"] = pd.to_datetime(weather_df["date"])
+print(weather_df.dtypes)
+
+
